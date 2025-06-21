f8908333801fe05887c19b0d1b2e5e8f4102b872
nabil ulil albab <nabilulilalbab78@gmail.com>
2025-06-21 23:57:14 +0700

    Initial commit for Django project

--- mountproject/settings.py
+++ mountproject/settings.py
@@ -32,7 +32,7 @@ CSRF_TRUSTED_ORIGINS = [
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*",'deployproject-production-e7b4.up.railway.app']
 
 
 # Application definition
